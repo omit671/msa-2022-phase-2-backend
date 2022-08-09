45e1550a74ce5a9e8223e6ac294c471f0b4aa13e
omit671 <omit671@aucklanduni.ac.nz>
2022-08-09 13:52:06 +1200

    Add title/description to Swagger docs

--- PokeBattleSim/Program.cs
+++ PokeBattleSim/Program.cs
@@ -5,7 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "PokeBattleSimAPI",
+        Description = "An API for creating a Pokemon team and fighting other teams."
+    });
+});
 
 var app = builder.Build();
 
